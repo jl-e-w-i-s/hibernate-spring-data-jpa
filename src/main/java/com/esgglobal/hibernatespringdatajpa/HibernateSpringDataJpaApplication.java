675f6730e9f3d769276522b7c84ee6a7d3fdda25
James Lewis <james.lewis@esgglobal.com>
2022-08-31 11:04:16 +0100

    initial commit using java 17 and spring-boot milestone version 3

---
+++ src/main/java/com/esgglobal/hibernatespringdatajpa/HibernateSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.esgglobal.hibernatespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSpringDataJpaApplication.class, args);
+    }
+
+}
