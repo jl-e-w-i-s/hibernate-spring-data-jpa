5bed9c0695749b7085f7b58ffe4992579d0097a2
James Lewis <james.lewis@esgglobal.com>
2022-09-01 10:34:09 +0100

    add a spring-boot test which loads the context via class attribute @SpringBootTest enabling @Autowired attribute to load the `BookRepository` and query the number of rows in the database table

--- src/test/java/com/esgglobal/hibernatespringdatajpa/HibernateSpringDataJpaApplicationTests.java
+++ src/test/java/com/esgglobal/hibernatespringdatajpa/HibernateSpringDataJpaApplicationTests.java
@@ -1,10 +1,23 @@
 package com.esgglobal.hibernatespringdatajpa;
 
+import com.esgglobal.hibernatespringdatajpa.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class HibernateSpringDataJpaApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testBookRepository_count_greaterThanOne() {
+        long count = bookRepository.count();
+        assertThat(count).isGreaterThan(1);
+    }
 
     @Test
     void contextLoads() {
